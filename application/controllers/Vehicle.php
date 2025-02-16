b97d17cf723315d0f719081cfd8eabdbb21face6
JuanFelixCespedes <juanfelixcespedes@gmail.com>
2025-02-16 17:13:18 -0500

    Update Vehicle.php

--- application/controllers/Vehicle.php
+++ application/controllers/Vehicle.php
@@ -34,7 +34,7 @@ class Vehicle extends CI_Controller {
 		$this->form_validation->set_rules('v_color','Vehicle Color','required|trim');
 		$testxss = xssclean($_POST);
 		if($this->form_validation->run()==TRUE && $testxss){
-			$response = $this->vehicle_model->add_vehicle($this->input->post());
+			$response=$this->vehicle_model->add_vehicle($this->input->post());
 			if($response) {
 				$this->session->set_flashdata('successmessage', 'New vehicle added successfully..');
 				redirect('vehicle');
