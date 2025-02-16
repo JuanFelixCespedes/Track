ca7b2086c9da409571c93538f77e341dae24bf67
JuanFelixCespedes <juanfelixcespedes@gmail.com>
2025-02-16 18:36:37 -0500

    Update Vehicle.php

--- application/controllers/Vehicle.php
+++ application/controllers/Vehicle.php
@@ -44,10 +44,7 @@ class Vehicle extends CI_Controller {
 			}
 		} else	{
 			$response = $this -> vehicle_model -> add_vehicle($this->input->post());
-			if( $response) {
-				$this->session->set_flashdata('successmessage', 'New vehicle added successfully..');
-				redirect('vehicle');
-			}
+			
 		}
 
 	}
